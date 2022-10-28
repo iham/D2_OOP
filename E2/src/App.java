a74da364c5c8c790f418d9d258d7b79cd2f4e71f
Markus Hilbert <markus.hilbert@iham.at>
2022-10-28 15:17:35 +0200

    adding args and a second project

---
+++ E2/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please insert a text to output: ");
+        String output = scanner.nextLine();
+
+        System.out.println("Please insert how many times you want to repeat that: ");
+        int multipler = scanner.nextInt();
+
+        System.out.println("The output is:");
+
+        for (int i = 0; i < multipler; i++) {
+            System.out.println(output);    
+        }
+
+        scanner.close();     
+    }
+}
